73895e16898acb29a126c65a2748bfc11220bd39
CodeTalker <jcsa031403@gmail.com>
2021-06-26 05:19:23 -0500

    HTML_structure

--- index.php
+++ index.php
@@ -9,6 +9,108 @@
 	<link rel="stylesheet" type="text/css" href="styles.css">
 </head>
 <body>
+<header>
+	<nav class="flex-container">
+		<a href="#" class="flex-item">About</a>
+		<a href="#" class="flex-item">Skillset</a>
+		<a href="#" class="flex-item">Portfolio</a>
+		<a href="#" class="flex-item">Contact</a>
+	</nav>
+</header>
 
+<h2>Hi! I'm Julio Salas</h2>
+<div class="grid-conteiner">	
+		<p class="grid-item"><strong></strong></p>
+		<p class="grid-item"><strong></strong></p>
+		<p class="grid-item"><strong></strong></p>
+		<img src="#">
+</div>
+
+<h2>I'm good in</h2>
+<div class="flex-container">
+		<div class="flex-item">
+			<img src="#">
+			<h3>Titulo</h3>
+		</div>
+		<div class="flex-item">
+			<img src="#">
+			<h3>Titulo</h3>
+		</div>
+		<div class="flex-item">
+			<img src="#">
+			<h3>Titulo</h3>
+		</div>
+		<div class="flex-item">
+			<img src="#">
+			<h3>Titulo</h3>
+		</div>
+		<div class="flex-item">
+			<img src="#">
+			<h3>Titulo</h3>
+		</div>
+		<div class="flex-item">
+			<img src="#">
+			<h3>Titulo</h3>
+		</div>
+</div>
+
+<h1>Portfolio</h1>
+<div class="grid-conteiner">
+		<div class="grid-item">
+			<img src="#">
+			<h3></h3>
+			<div>inline</div>
+		</div>
+		<div class="grid-item">
+			<img src="#">
+			<h3></h3>
+			<div>inline</div>
+		</div>
+		<div class="grid-item">
+			<img src="#">
+			<h3></h3>
+			<div>inline</div>
+		</div>
+		<div class="grid-item">
+			<img src="#">
+			<h3></h3>
+			<div>inline</div>
+		</div>
+		<div class="grid-item">
+			<img src="#">
+			<h3></h3>
+			<div>inline</div>
+		</div>
+		<div class="grid-item">
+			<img src="#">
+			<h3></h3>
+			<div>inline</div>
+		</div>
+</div>
+
+<h2>Contact</h2>
+<form class="grid-conteiner">
+	<p>Name</p>
+	<input class="grid-item" type="text" name="">
+	<p>Asunto</p>
+	<input class="grid-item" type="text" name="">
+	<p>Message</p>
+	<input class="grid-item" type="text" name="">
+</form>
+
+<div class="grid-conteiner">
+	<div class="grid-item">
+		<img src="#">
+		<h3>Title</h3>
+	</div>
+	<div class="grid-item">
+		<img src="#">
+		<h3>Title</h3>
+	</div>
+	<div class="grid-item">
+		<img src="#">
+		<h3>Title</h3>
+	</div>
+</div>
 </body>
 </html>
