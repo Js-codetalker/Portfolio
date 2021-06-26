b3d16faa2dcf85d665d79d66b1e51987a7965b7b
CodeTalker <jcsa031403@gmail.com>
2021-06-26 13:48:10 -0500

    background added

--- index.php
+++ index.php
@@ -17,99 +17,101 @@
 		<a href="#Contact"><p>Contact</p><div class="gradient"></div></a>
 	</nav>
 </header>
-
-<h2 id="About">Hi! I'm Julio Salas</h2>
-<div class="grid-conteiner">	
+<div class="container">
+	<h2 id="About"><strong>Hi!</strong> I'm Julio Salas</h2>
+	<div class="grid-conteiner">	
 		<p class="grid-item">the "nerd" behind this website, and <strong>the person you've looking for</strong> carry out your proyect.</p>
 		<p class="grid-item">I'm a web designer from Venezuela, but actually based on Colombia. I like reading, coding and <strong>solve problems.</strong></p>
 		<p class="grid-item">But of course, you want proofs rather than words, so keep scrolling and <strong>see what I can do for you.</strong></p>
 		<img src="#">
-</div>
-
-<h2 id="Skills">I'm good in</h2>
-<div class="flex-container">
-		<div class="flex-item">
-			<img src="#">
-			<h3>Titulo</h3>
-		</div>
-		<div class="flex-item">
-			<img src="#">
-			<h3>Titulo</h3>
-		</div>
-		<div class="flex-item">
-			<img src="#">
-			<h3>Titulo</h3>
-		</div>
-		<div class="flex-item">
-			<img src="#">
-			<h3>Titulo</h3>
-		</div>
-		<div class="flex-item">
-			<img src="#">
-			<h3>Titulo</h3>
-		</div>
-		<div class="flex-item">
-			<img src="#">
-			<h3>Titulo</h3>
-		</div>
-</div>
-
-<h1 id="Portfolio">Portfolio</h1>
-<div class="grid-conteiner">
-		<div class="grid-item">
-			<img src="#">
-			<h3></h3>
-			<div>inline</div>
-		</div>
-		<div class="grid-item">
-			<img src="#">
-			<h3></h3>
-			<div>inline</div>
-		</div>
-		<div class="grid-item">
-			<img src="#">
-			<h3></h3>
-			<div>inline</div>
-		</div>
-		<div class="grid-item">
-			<img src="#">
-			<h3></h3>
-			<div>inline</div>
-		</div>
-		<div class="grid-item">
-			<img src="#">
-			<h3></h3>
-			<div>inline</div>
-		</div>
-		<div class="grid-item">
-			<img src="#">
-			<h3></h3>
-			<div>inline</div>
-		</div>
-</div>
-
-<h2 id="Contact">Contact</h2>
-<form class="grid-conteiner">
-	<p>Name</p>
-	<input class="grid-item" type="text" name="">
-	<p>Asunto</p>
-	<input class="grid-item" type="text" name="">
-	<p>Message</p>
-	<input class="grid-item" type="text" name="">
-</form>
-
-<div class="grid-conteiner">
-	<div class="grid-item">
-		<img src="#">
-		<h3>Title</h3>
 	</div>
-	<div class="grid-item">
-		<img src="#">
-		<h3>Title</h3>
+
+
+	<h2 id="Skills">I'm good in</h2>
+	<div class="flex-container">
+		<div class="flex-item">
+			<img src="#">
+			<h3>Titulo</h3>
+		</div>
+		<div class="flex-item">
+			<img src="#">
+			<h3>Titulo</h3>
+		</div>
+		<div class="flex-item">
+			<img src="#">
+			<h3>Titulo</h3>
+		</div>
+		<div class="flex-item">
+			<img src="#">
+			<h3>Titulo</h3>
+		</div>
+		<div class="flex-item">
+			<img src="#">
+			<h3>Titulo</h3>
+		</div>
+		<div class="flex-item">
+			<img src="#">
+			<h3>Titulo</h3>
+		</div>
 	</div>
-	<div class="grid-item">
-		<img src="#">
-		<h3>Title</h3>
+
+	<h1 id="Portfolio">Portfolio</h1>
+	<div class="grid-conteiner">
+			<div class="grid-item">
+				<img src="#">
+				<h3></h3>
+				<div>inline</div>
+			</div>
+			<div class="grid-item">
+				<img src="#">
+				<h3></h3>
+				<div>inline</div>
+			</div>
+			<div class="grid-item">
+				<img src="#">
+				<h3></h3>
+				<div>inline</div>
+			</div>
+			<div class="grid-item">
+				<img src="#">
+				<h3></h3>
+				<div>inline</div>
+			</div>
+			<div class="grid-item">
+				<img src="#">
+				<h3></h3>
+				<div>inline</div>
+			</div>
+			<div class="grid-item">
+				<img src="#">
+				<h3></h3>
+				<div>inline</div>
+			</div>
+	</div>
+
+	<h2 id="Contact">Contact</h2>
+	<form class="grid-conteiner">
+		<p>Name</p>
+		<input class="grid-item" type="text" name="">
+		<p>Asunto</p>
+		<input class="grid-item" type="text" name="">
+		<p>Message</p>
+		<input class="grid-item" type="text" name="">
+	</form>
+
+	<div class="grid-conteiner">
+		<div class="grid-item">
+			<img src="#">
+			<h3>Title</h3>
+		</div>
+		<div class="grid-item">
+			<img src="#">
+			<h3>Title</h3>
+		</div>
+		<div class="grid-item">
+			<img src="#">
+			<h3>Title</h3>
+		</div>
 	</div>
 </div>
 </body>
