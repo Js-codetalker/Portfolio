caeaf42947177cfeb7fdc41e0b3d10025347fa8c
CodeTalker <jcsa031403@gmail.com>
2021-06-29 11:03:15 -0500

    responsive

--- app.js
+++ app.js
@@ -1,14 +1,40 @@
 let btnOpenPopup = document.getElementById('portfolio__item-viewMore'),
 	overlay = document.getElementById('overlay'),
 	btnClosePopup = document.getElementById('close-popup'),
-	popup = document.getElementById('popup')
+	popup = document.getElementById('popup');
 
 btnOpenPopup.addEventListener('click',function() {
 	overlay.classList.add('active');
-	popup.classList.add('active')
+	popup.classList.add('active');
 });
 
 btnClosePopup.addEventListener('click',function() {
-	overlay.classList.remove('active')
-	popup.classList.remove('active')
+	overlay.classList.remove('active');
+	popup.classList.remove('active');
+});
+
+//Responsive menu
+
+function openAndCloseMenu() {
+	navSection.classList.toggle("opened")
+	navSection.classList.toggle("closed")
+}
+
+let responsiveMenuButton = document.getElementById('responsiveMenu__button'),
+	navSection = document.getElementById('navSection'),
+	link1 = document.getElementById('link1'),
+	link2 = document.getElementById('link2'),
+	link3 = document.getElementById('link3');
+
+responsiveMenuButton.addEventListener('click',function () {
+	openAndCloseMenu();
+});
+link1.addEventListener('click',function() {
+	openAndCloseMenu();
+});
+link2.addEventListener('click', function(){
+	openAndCloseMenu();
+});
+link3.addEventListener('click', function(){
+	openAndCloseMenu();
 });
--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet">
 	<meta charset="utf-8">
 	<title></title>
 	<link rel="stylesheet" type="text/css" href="styles.css">
@@ -13,18 +14,23 @@
 </head>
 <body>
 <header>
-	<nav>
-		<a href="#About"><p>About</p><div class="gradient"></div></a>
-		<a href="#Portfolio"><p>Portfolio</p><div class="gradient"></div></a>
-		<a href="#Contact"><p>Contact</p><div class="gradient"></div></a>
+	<div class="responsiveMenu">
+		<img id="responsiveMenu__button" src="Images/burgerMenu.jpg">
+	</div>
+	<nav id="navSection" class="closed">
+		<a href="#About" id="link1"><p>About</p><div class="gradient"></div></a>
+		<a href="#Portfolio" id="link2"><p>Portfolio</p><div class="gradient"></div></a>
+		<a href="#Contact" id="link3"><p>Contact</p><div class="gradient"></div></a>
 	</nav>
 </header>
 	<div class="blur-filter">
-		<h2 id="About"><strong>Hi!</strong> I'm Julio Salas</h2>
-		<div class="grid-conteiner">	
-			<p class="grid-item">the "nerd" behind this website, and <strong>the person you've looking for</strong> 	carry out your proyect.</p>
-			<p class="grid-item">I'm a web designer from Venezuela, but actually based on Colombia. I like reading, coding and <strong>solve problems.</strong></p>
-			<p class="grid-item">But of course, you want proofs rather than words, so keep scrolling and <strong>see 	what I can do for you.</strong></p>
+		<div class="about">
+			<h2 id="About" class="about__title"><strong>Hi!</strong> I'm Julio Salas</h2>
+				<p class="about__paragraph">I'm the "nerd" behind this website, and <strong>the person you've looking for</strong> 	carry out your proyect.</p>
+				<br>
+				<p class="about__paragraph">I'm a web designer from Venezuela, but actually based on Colombia. I like reading, coding and <strong>solve problems.</strong></p>
+				<br>
+				<p class="about__paragraph">But of course, you want proofs rather than words, so keep scrolling and <strong>see 	what I can do for you.</strong></p>
 		</div>
 
 		<h1 id="Portfolio">Portfolio</h1>
@@ -58,11 +64,11 @@
 			</div>
 		</form>
 		<div class="socialMedia">
-				<a class="socialMedia__slot" href="https://www.linkedin.com/in/julio-salas-b10605200/">
+				<a target="blank" class="socialMedia__slot" href="https://www.linkedin.com/in/julio-salas-b10605200/">
 					<img src="Images/linkedin.png" alt="linkedin-icon">
 					<p>Message me on</p>
 				</a>
-				<a class="socialMedia__slot" href="https://github.com/Js-codetalker">
+				<a target="blank" class="socialMedia__slot" href="https://github.com/Js-codetalker">
 					<img src="Images/github.png" alt="Github-icon">
 					<p>Follow me on</p>
 				</a>
